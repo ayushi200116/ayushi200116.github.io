2dc9da1ae844c71d07567e284c5daee4a2e35a4e
Ayushi Indu <singhayushi535@gmail.com>
2025-07-06 22:19:39 +0530

    updated code

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1af1cd4da6d2298e0b3bf83b7d472829.js"
+  "./precache-manifest.cddb21064b3ae94cd7f574977b3bdc32.js"
 );
 
 self.addEventListener('message', (event) => {
